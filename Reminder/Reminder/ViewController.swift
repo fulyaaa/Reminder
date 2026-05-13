a36c45a977f8a9f6d0f739bb7ee4270f2fc8cd06
Fulya Akan <fulyaakan@outlook.com>
2026-05-13 19:48:50 +0300

    Setup deployment targets

---
+++ Reminder/Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by fulya akan on 13.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
